f0703a7cbeedda65b1afed96781bfb972fb3a54a
rplkita <satriayudha01@gmail.com>
2023-05-27 21:05:10 +0700

    menambahan halaman home about course

--- todoList/routes/web.php
+++ todoList/routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/course', function () {
+    return view('course');
 });
